ac9256ec29a9e1083c40c8e56001848d9bdd992b
Gandhi naik <ggnaik2006@gmail.com>
2026-08-08 23:13:00 +0530

    Add files via upload

---
+++ charat.java
@@ -0,0 +1,35 @@
+class charat{
+    public static void main(String[] args) {
+        String name="Gandhi";
+        // String lastname="Naik";
+        // String fullname=firstname + " " + lastname;
+        // System.out.print("My name is:"+fullname);
+
+
+
+
+        char firstchar=name.charAt(0);
+
+
+        System.out.println(firstchar);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
+    }
+}
