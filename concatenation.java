fd1145e93edbc0da0e0a4c0ae43374b8f12f8862
Gandhi naik <ggnaik2006@gmail.com>
2026-08-08 23:02:42 +0530

    Add files via upload

---
+++ concatenation.java
@@ -0,0 +1,9 @@
+class concatenation{
+    public static void main(String[] args) {
+        String firstname="Gandhi";
+        String lastname="Naik";
+        String fullname=firstname + " " + lastname;
+        System.out.print("My name is:"+fullname);
+         
+    }
+}
