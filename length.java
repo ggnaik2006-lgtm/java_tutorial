a945e22e45e7337e4f3869be7190cfbd56de39f3
Gandhi naik <ggnaik2006@gmail.com>
2026-08-08 22:54:09 +0530

    Add files via upload

---
+++ length.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class length{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the string:");
+        String inputstString=sc.nextLine();
+        System.out.println("My name is:" + inputstString.length());
+    }
+}
